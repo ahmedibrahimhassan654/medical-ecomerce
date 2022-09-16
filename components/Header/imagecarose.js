6a1483e6a79d179a2379b56fe18cfe22b9b27f0d
Ahmed  Ibrahim <ahmedibrahimhassan654@gmail.com>
2022-09-16 02:17:45 +0200

    delete some npms

--- components/Header/imagecarose.js
+++ components/Header/imagecarose.js
@@ -2,16 +2,14 @@ import * as React from "react";
 import { useTheme } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import MobileStepper from "@mui/material/MobileStepper";
-import Paper from "@mui/material/Paper";
-import Typography from "@mui/material/Typography";
+
 import Button from "@mui/material/Button";
 import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
 import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
-import { Carousel } from "react-responsive-carousel";
+
 import SwipeableViews from "react-swipeable-views";
 import { autoPlay } from "react-swipeable-views-utils";
-import { Grid } from "@mui/material";
-import { Colors } from "../../styles/colors";
+
 
 const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
